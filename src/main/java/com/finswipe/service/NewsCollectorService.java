e1f47c77fdfaee9a249cf582e31ad6e578ed1509
Ko JeongGyun <swj0718820@gmail.com>
2026-05-14 10:14:34 +0900

    NewsCollectorService: 미사용 @Transactional import 제거

--- src/main/java/com/finswipe/service/NewsCollectorService.java
+++ src/main/java/com/finswipe/service/NewsCollectorService.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientResponseException;
 
